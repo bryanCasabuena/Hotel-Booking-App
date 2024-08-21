89f853d5885ececff902af0241699802f56a7f23
bryanCasabuena <bryancasabuenac@gmail.com>
2024-08-21 15:16:00 +0800

    creating instances

--- main.py
+++ main.py
@@ -1,1 +1,29 @@
-print("asdas")
+import pandas as pd
+
+
+df = pandas.read_csv("hotels.csv")
+class Hotel:
+    def __init__(self, id):
+        pass
+    
+    def book(self):
+        pass
+    
+    def available(self):
+        pass
+
+class ReservationTicket:
+    def __init__(self, name, hotel):
+    def generate(self):
+        pass
+    
+print((df))
+id = input("Enter the id of the hotel: ")
+hotel = Hotel(id)
+
+if hotel.available():
+    hotel.book()
+    name = input("Enter your name: ")
+    reservation_ticket = ReservationTicket(name, hotel)
+    
+    
